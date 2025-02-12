4f6b7c2859a4146a00bdb3dd641ce37cad84f8d5
wuthirri <182341417+wuthirri@users.noreply.github.com>
2025-02-12 11:07:50 +1030

    Made changes to environment file for setting up and moved files into sub directories

--- scripts/01_download/download.sh
+++ scripts/01_download/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Move to downloads directory
-cd ../../downloads
+cd ../../data/reference
 
 # Change to downloads folder and Download the B10K assembly from NCBI 
 curl -O https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/013/399/875/GCA_013399875.1_ASM1339987v1/GCA_013399875.1_ASM1339987v1_genomic.fna.gz

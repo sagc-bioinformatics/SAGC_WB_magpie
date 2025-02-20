26e5e4420c3b08fd6495e92bf6c72e47355202ce
John Salamon <john.salamon@sahmri.com>
2025-02-20 15:37:43 +1030

    add launch script for SAHMRI hpc

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -8,5 +8,6 @@ nextflow run pipeline.nf \
     --samples='../downloads/samples/*R[1,2]*.fastq.gz' \
     --genome='../downloads/reference/GCA_013399875.1_ASM1339987v1_genomic.fna.gz' \
     --annotation='../downloads/reference/GCA_013399875.1_ASM1339987v1_genomic.gtf.gz' \
+    -resume
 
 
